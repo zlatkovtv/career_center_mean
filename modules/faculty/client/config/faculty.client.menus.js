17b61b3acca74470060f024eaa4d604c4fd186c2
Konstantin Zlatkov <konstantin.zlatkov@atos.net>
2017-12-19 14:23:41 +0200

    visual improvement for job detail view

--- modules/faculty/client/config/faculty.client.menus.js
+++ modules/faculty/client/config/faculty.client.menus.js
@@ -10,7 +10,7 @@
   function menuConfig(menuService) {
     // Set top bar menu items
     menuService.addMenuItem('topbar', {
-      title: 'View and edit student profiles',
+      title: 'Manage classes',
       state: 'faculty',
       roles: ['faculty'],
       position: 1
