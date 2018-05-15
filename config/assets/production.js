cab244c87a8d19a830460ee00226429a7bcb6d98
Konstantin Zlatkov <konstantin.zlatkov@atos.net>
2018-05-15 15:42:29 +0300

    production

--- config/assets/production.js
+++ config/assets/production.js
@@ -7,10 +7,9 @@ module.exports = {
     lib: {
       css: [
         // bower:css
-        'public/lib/bootstrap/dist/css/bootstrap.min.css',
         'public/lib/angular-ui-notification/dist/angular-ui-notification.min.css',
-        'public/lib/tr-ng-grid/trNgGrid.min.css',
-        'public/lib/bootstrap-toggle/css/bootstrap-toggle.min.css'
+        'public/lib/bootstrap/dist/css/bootstrap.min.css',
+        'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
         // endbower
       ],
       js: [
@@ -18,16 +17,13 @@ module.exports = {
         'public/lib/angular/angular.min.js',
         'public/lib/angular-animate/angular-animate.min.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
-        'public/lib/jquery/dist/jquery.min.js',
-        'public/lib/bootstrap-toggle/js/bootstrap-toggle.min.js',
-        'public/lib/ng-file-upload/ng-file-upload.min.js',
-        'public/lib/angular-messages/angular-messages.js',
+        'public/lib/angular-messages/angular-messages.min.js',
         'public/lib/angular-mocks/angular-mocks.js',
         'public/lib/angular-resource/angular-resource.min.js',
         'public/lib/angular-ui-notification/dist/angular-ui-notification.min.js',
         'public/lib/angular-ui-router/release/angular-ui-router.min.js',
+        'public/lib/ng-file-upload/ng-file-upload.min.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
-        'public/lib/tr-ng-grid/trNgGrid.min.js',
         // endbower
       ]
     },
